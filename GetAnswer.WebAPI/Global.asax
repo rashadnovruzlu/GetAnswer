4c310f6ff304fd0558611f1f81004721199a282b
Rashad Novruzlu <rashad.ny@bestcomp.net>
2019-02-11 23:19:41 +0400

    ...

---
+++ GetAnswer.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GetAnswer.WebAPI.WebApiApplication" Language="C#" %>
---
+++ GetAnswer.WebAPI/Services/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace GetAnswer.WebAPI.Services
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            await configSendGridasync(message);
+        }
+
+        // Use NuGet to install SendGrid (Basic C# client lib) 
+        private async Task configSendGridasync(IdentityMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
